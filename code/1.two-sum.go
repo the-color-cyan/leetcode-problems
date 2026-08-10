ef0c5ed7850df6e88cafe45e8e8bcae7c8240e8c
cyan <cyan@cyan.fish>
2026-08-10 16:49:56 -0500

    go: 1 & 2

--- code/1.two-sum.go
+++ code/1.two-sum.go
@@ -1,16 +1,16 @@
-package leetcode
-
 func twoSum(nums []int, target int) []int {
 	nMap := make(map[int]int)
 
 	for i, n := range nums {
 		diff := target - n
-		diffIndex := nMap[diff]
+		diffIndex, ok := nMap[diff]
 
-		if diffIndex != nil {
-			return [2]int{diffIndex, i}
+		if ok {
+			return []int{diffIndex, i}
 		}
 
 		nMap[n] = i
 	}
+
+	return []int{}
 }
