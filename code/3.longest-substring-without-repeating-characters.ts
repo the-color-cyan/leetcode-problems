6957e9274e37b610b0183536c0cb311d082afea2
cyan <cyan@cyan.fish>
2026-08-11 23:27:09 -0500

    ts: 3 - spelling

--- code/3.longest-substring-without-repeating-characters.ts
+++ code/3.longest-substring-without-repeating-characters.ts
@@ -1,16 +1,16 @@
 function lengthOfLongestSubstring(s: string): number {
-    // index of last occurance of a character
-    let lastOccurance = new Map<string, number>();
+    // index of last occurrence of a character
+    let lastOccurrence = new Map<string, number>();
     let longest = 0;
     let start = 0;
 
     for (let end = 0; end < s.length; end++) {
-        if (lastOccurance.has(s[end])) {
-            const currLastOccurance = lastOccurance.get(s[end]);
-            if (currLastOccurance + 1 > start) start = currLastOccurance + 1;
+        if (lastOccurrence.has(s[end])) {
+            const currLastOccurrence = lastOccurrence.get(s[end]);
+            if (currLastOccurrence + 1 > start) start = currLastOccurrence + 1;
         }
 
-        lastOccurance.set(s[end], end);
+        lastOccurrence.set(s[end], end);
 
         const currSubstringLength = end + 1 - start;
         if (currSubstringLength > longest) longest = currSubstringLength;
