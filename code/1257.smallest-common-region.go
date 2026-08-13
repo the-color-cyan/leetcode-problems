17ecd955d493e7e4720b704a5633846aabe32f41
cyan <cyan@cyan.fish>
2026-08-13 13:06:52 -0500

    go: 1257

--- code/1257.smallest-common-region.go
+++ code/1257.smallest-common-region.go
@@ -1,7 +1,7 @@
-package leetcode
-
-func findSmallestRegion(source [][]RegionName, region1 RegionName, region2 RegionName) string {
+func findSmallestRegion(source [][]string, region1 string, region2 string) string {
 	regions := mapRegions(source)
+
+	return string(regions.LCA(RegionName(region1), RegionName(region2)))
 }
 
 type RegionName string
@@ -9,14 +9,50 @@ type RegionName string
 // maps each non-root region to its direct parent
 type Regions map[RegionName]RegionName
 
-func mapRegions(source [][]RegionName) *Regions {
+type Set[T comparable] map[T]struct{}
+
+func (s Set[T]) Add(val T) {
+	s[val] = struct{}{}
+}
+
+func (s Set[T]) Has(val T) bool {
+	_, exists := s[val]
+	return exists
+}
+
+func mapRegions(source [][]string) *Regions {
 	regions := make(Regions)
 
 	for _, region := range source {
 		for _, child := range region[1:] {
-			regions[child] = region[0]
+			regions[RegionName(child)] = RegionName(region[0])
 		}
 	}
 
 	return &regions
+}
+
+func (r Regions) LCA(r1, r2 RegionName) RegionName {
+	r1Parents := make(Set[RegionName])
+
+	for parent := r1; parent != ""; parent = r[parent] {
+		r1Parents.Add(parent)
+	}
+
+	result := r2
+	for !r1Parents.Has(result) {
+		result = r[result]
+	}
+
+	return result
+}
+
+func findRoot(regions *Regions) RegionName {
+	for region, parent := range *regions {
+		if parent == "" {
+			return region
+		}
+	}
+
+	panic("cannot find root")
 }
