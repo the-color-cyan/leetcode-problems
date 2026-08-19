553a03bb09060bdf465a8ec463b52cbeb715d39d
cyan <cyan@cyan.fish>
2026-08-19 17:04:07 -0500

    go: 1386 partial

--- code/1386.cinema-seat-allocation.go
+++ code/1386.cinema-seat-allocation.go
@@ -1,17 +1,18 @@
 package leetcode
 
 const (
-	BlockSize int = 4
-	Blocks    int = 3
+	blockSize   int = 4
+	blocks      int = 3
+	seatsPerRow int = 10
 )
 
-var SeatBlocks = [Blocks]SeatBlock{
+var seatBlocks = [blocks]SeatBlock{
 	{2, 3, 4, 5},
 	{4, 5, 6, 7},
 	{6, 7, 8, 9},
 }
 
-type SeatBlock [BlockSize]int
+type SeatBlock [blockSize]int
 
 // type Set[T comparable] map[T]struct{}
 //
@@ -23,6 +24,15 @@ type SeatBlock [BlockSize]int
 // 	_, exists := s[val]
 // 	return exists
 // }
+
+func groupFitMap(seatBlocks []SeatBlock) map[int]int {
+	fitMap := make(map[int]int)
+
+	for _, block := range seatBlocks {
+		for _, seat := range block {
+		}
+	}
+}
 
 func maxNumberOfFamilies(n int, reservedSeats [][]int) int {
 }
